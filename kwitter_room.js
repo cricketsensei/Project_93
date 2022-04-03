4e3f0d1497eb352d64a4f8cfde1d276418eff585
cricketsensei <93299002+cricketsensei@users.noreply.github.com>
2022-04-03 09:44:20 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANBuiCyJjk29w0bl9Qp3mFwM3mms4UreQ",
+    authDomain: "kwitter-project-ba8d9.firebaseapp.com",
+    projectId: "kwitter-project-ba8d9",
+    storageBucket: "kwitter-project-ba8d9.appspot.com",
+    messagingSenderId: "214192579971",
+    appId: "1:214192579971:web:8860800b14d225bbff82b6",
+    measurementId: "G-S7LP6WRM4X"
+  };
+  
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
