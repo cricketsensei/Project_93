e4286f142a7954468348786ef8783e2cee2bc5c7
cricketsensei <93299002+cricketsensei@users.noreply.github.com>
2022-04-18 20:51:06 -0700

    Delete kwitter.js

--- kwitter.js
+++
@@ -1,6 +0,0 @@
-var user = "";
-function addUser(){
-    user = document.getElementById("user_name").value;
-    localStorage.setItem("Username", user);
-    window.location = "kwitter_room.html";
-}
